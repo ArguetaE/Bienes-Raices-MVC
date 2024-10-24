49f0be2225819709c823437afe58a4530c79d4be
Jose Argueta <josearguetaespinoza@gmail.com>
2024-10-24 00:53:41 -0600

    Asociando Urls

--- Router.php
+++ Router.php
@@ -3,8 +3,26 @@
 namespace MVC;
 
 class Router {
-    public function __construct()
-    {
-        echo "creando el Router";
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas(){
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if ($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? Null;
+        }
+        if ($fn) {
+            // La URL existe y hay una funcion asociada
+            call_user_func($fn, $this);
+        } else {
+            echo "pagina no encontrada";
+        }
     }
 }
